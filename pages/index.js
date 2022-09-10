9eb35e16fd1c7c8a6c048bbb85788887ec87f61c
fernandopbarboza <fernando.p.barboza@gmail.com>
2022-09-10 17:41:28 -0300

    Components

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,6 @@
-import React from "React";
-import { ReactDOM } from "React.DOM";
-
-import "../src/css/base.css";
+import React,{Component} from "React";
 import Header from "./Components/Header";
+import "../src/css/base.css";
 
 function App(){
     
