9ee67ee34aa7bf143c9e7b30b0f93d56653d5f9d
fernandopbarboza <fernando.p.barboza@gmail.com>
2022-09-10 14:25:09 -0300

    Componentes React

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,24 @@
-import {useState} from 'react';
 import Link from 'next/link';
+import "../src/css/base.css";
+import "../src/css/vendor.css";
+import "../src/css/main.css";
+import "../src/js/jquery-3.2.1.min.js";
+import "../src/js/plugins.js";
+import "../src/js/main.js";
+import "../src/js/modernizr.js";
+import "../src/js/pace.min.js";
+import Header from "./Header";
+import Home from "./Home";
+import About from "./About";
+import AboutHow from "./AboutHow";
 
-function Home() {
+
+function Home(){
+    
     return (
+        
     <div>
+    
         <div>Home</div>
         <Link href='/sobre'>
             <a >Acessar pagina sobre</a>
