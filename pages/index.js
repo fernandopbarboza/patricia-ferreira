4169eccc30f0b3cf798c82e54ceaae00c06cb110
fernandopbarboza <fernando.p.barboza@gmail.com>
2022-09-10 14:58:43 -0300

    Ajuste Index

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,4 @@
-import Link from 'next/link';
 import "../src/css/base.css";
-import "../src/css/vendor.css";
-import "../src/css/main.css";
-import "../src/js/jquery-3.2.1.min.js";
-import "../src/js/plugins.js";
-import "../src/js/main.js";
-import "../src/js/modernizr.js";
-import "../src/js/pace.min.js";
 import Header from "./Header";
 
 function App(){
